979615c1bc77c23d366ab031bf59880e9d6f36f8
Jester Santos <santosjester738@gmail.com>
2024-04-21 05:14:26 -0500

    Fix deprecated lsps

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,11 +7,13 @@ local config = function()
 			null_ls.builtins.formatting.stylua,
             null_ls.builtins.diagnostics.rubocop,
             null_ls.builtins.formatting.rubocop,
-            null_ls.builtins.diagnostics.eslint_d,
+            -- null_ls.builtins.diagnostics.eslint_d, (DEPRECATED)
+            require("none-ls.diagnostics.eslint_d"), -- replacement
             null_ls.builtins.formatting.prettier,
             null_ls.builtins.formatting.black,
             null_ls.builtins.formatting.isort,
-            null_ls.builtins.diagnostics.cpplint,
+            -- null_ls.builtins.diagnostics.cpplint, (DEPRECATED)
+            require("none-ls.diagnostics.cpplint"), -- replacement
 		},
 	})
 
@@ -21,5 +23,8 @@ end
 return {
 	-- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua.
 	"nvimtools/none-ls.nvim",
+    dependencies = {
+        "nvimtools/none-ls-extras.nvim", -- for deprecated
+    },
 	config = config,
 }
